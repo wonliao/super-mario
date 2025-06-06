42c47c19e5230edbf5930821ae6671323b953f08
wonliao <wonliao1117@gmail.com>
2025-06-06 14:06:32 +0800

    調整獎項數量為五個

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 
 app = Flask(__name__)
 
-PRIZES = [100, 500, 1000, 5000, 10000, 20000, 50000, 100000, 200000, 1000000]
+PRIZES = [100, 500, 1000, 5000, 10000]
 
 @app.route('/')
 def index():
